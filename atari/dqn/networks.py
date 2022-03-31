173d66c046ac71ed767273728f97b5069840ef36
NNHieu <ngochieutb13@gmail.com>
2022-03-31 16:08:33 +0700

    atari dqn baseline

---
+++ atari/dqn/networks.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+# ALGO LOGIC: initialize agent here:
+class BaselineQNetwork(nn.Module):
+    def __init__(self, env):
+        super(BaselineQNetwork, self).__init__()
+        self.pre = nn.Sequential(
+            nn.Conv2d(4, 32, 8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(),
+        )
+        self.core = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(),
+        )
+        self.post = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, env.single_action_space.n),
+        )
+
+    def forward(self, x):
+        out = self.pre(x / 255.0)
+        out = self.core(out)
+        out = self.post(out)
+        return out
